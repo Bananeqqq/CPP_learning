e62a264fec1524ad3987560124d68e4c03767e8d
Bananeqqq <66287707+Bananeqqq@users.noreply.github.com>
2022-05-29 23:38:31 +0200

    Add files via upload

---
+++ __C++_Template_project/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<limits>
+
+
+int main(){
+    int a;
+
+/*    if (!std::cin){               //If fail (cin >> int b ("Adkdk") == fail)
+        std::cin.clear(); 
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+*/
+
+
+
+
+}
